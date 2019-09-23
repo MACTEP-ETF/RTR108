7ece1c6f7fa2b56feee9f00c79b73235f22074ae
user <vfedorovich@yandex.ru>
2019-09-23 18:19:11 +0300

    20190923_18_19

---
+++ darbi/04.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+float pi ()
+ {
+ float pi=3.14;
+ return pi;
+ }
+
+int main()
+ {
+//apliecības numurs 041RDB182
+ int R=41;
+ float L=2*pi()*R;
+ printf ("Rinka ar rādiusu %d garums ir %.2f m\n", R, L);
+
+ }
