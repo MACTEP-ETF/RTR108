6daebfe269934787bfde41e2a6c3cd2e781fbf43
MACTEP-ETF <55091350+MACTEP-ETF@users.noreply.github.com>
2019-11-21 01:30:00 +0200

    Add files via upload

--- darbi/L14/42.c
+++ darbi/L14/42.c
@@ -8,7 +8,7 @@
 float mediaanas_meklesana(int mas3[], int MasivaIzmers) // medianas apreekinaasana
 {
     float mediaana = 0;
-    if(MasivaIzmers%2 == 0) // ja elementu skaits ir paara
+    if (MasivaIzmers%2 == 0) // ja elementu skaits ir paara
         mediaana = (mas3[(MasivaIzmers-1)/2] + mas3[MasivaIzmers/2])/2.0;
     else // ja elementu skaits ir nepaara
         mediaana = mas3[MasivaIzmers/2];
@@ -19,11 +19,11 @@ int moda(int mas3[],int MasivaIzmers)
 {
    int maxVertiba = 0, maxSkaits = 0, i, j;
 
-   for (i = 0; i < MasivaIzmers; ++i)
+   for (i=0; i<MasivaIzmers; ++i)
    {
       int skaititajs = 0;
 
-      for (j = 0; j < MasivaIzmers; ++j)
+      for (j=0; j<MasivaIzmers; ++j)
       {
          if (mas3[j] == mas3[i])
          ++skaititajs;
@@ -45,19 +45,20 @@ int moda(int mas3[],int MasivaIzmers)
 
 int main()
 {
-    int z, i, j, k, l; // ciklu mainigie
-    int m; // Kopas izmeers ko ievadiis lietotajs
+    int i, j, k, l, z; // ciklu mainigie
+    int min, max;
     float sum, mediaana = 0;
     int Temp; // iislaiciigais mainiigais
     int mas3[20] = {86, 108, 97, 100, 105, 109, 105, 114, 115, 0, 70, 101, 100, 111, 114, 111, 118, 105, 99, 115}; //kaartojamo skaitlju masiivs
     int MasivaIzmers = sizeof(mas3)/sizeof(int);
 
-    printf("Nesakaartota masiiva burtu un atbilstosha ASCII koda veidaa:\n");
+/*    printf("Nesakaartota masiiva burtu un atbilstosha ASCII koda veidaa:\n");
     for (z=0; z<(sizeof(mas3)/sizeof(int)); z++) // izvada NESAKAARTOTA masiiva burtu veidaa un atbistosu ASCII koda veidaa
         if (mas3[z] == 0)
             printf("Simbols: 'atstarpe'\tASCII kods: %d\n", mas3[z]);
         else
             printf("Simbols: %c\t\tASCII kods: %d\n", mas3[z], mas3[z]);
+*/
 
     for (i=0; i<MasivaIzmers; i++) // cikliska masiiva kaartoshana ar burbuli
     {
@@ -73,17 +74,34 @@ int main()
     }
 
     printf("\n\nSakaartota masiiva burtu un atbilstosha ASCII koda veidaa:\n");
-    for (z=0; z<MasivaIzmers; z++) // izvada SAKAARTOTA masiiva burtu veidaa un atbistosu ASCII koda veidaa
+    for (z=0; z<MasivaIzmers; z++) // izvada SAKAARTOTA masiiva burtu un atbistosu ASCII koda veidaa
         if (mas3[z] == 0)
             printf("Simbols: 'atstarpe'\tASCII kods: %d\n", mas3[z]);
         else
             printf("Simbols: %c\t\tASCII kods: %d\n", mas3[z], mas3[z]);
 
 
+    min = mas3[0];
+    for (i=0; i<MasivaIzmers; i++)
+    {
+	if (mas3[i] < min)
+	min = mas3[i];
+	else continue;
+    }
+
+    max = mas3[0];
+    for (i=0; i<MasivaIzmers; i++)
+    {
+	if (mas3[i] > max)
+	max = mas3[i];
+	else continue;
+    }
+
+
     printf("\n\n[Statistiskie raadiitaaji]:\n");
-    printf("(a) Masiiva izmeers: %d\n", sizeof(mas3)/sizeof(int));
-    printf("(b) Minimaala elementa veertiiba: Pagaidam nav aprekinats \n");
-    printf("(c) Maksimaala elementa veertiiba: Pagaidam nav aprekinats \n");
+    printf("(a) Masiiva izmeers: %d\n", MasivaIzmers);
+    printf("(b) Masiiva minimaala veertiiba: %d\n", min);
+    printf("(c) Masiiva maksimaala veertiiba: %d\n", max);
 
     sum = 0;
     for (l=0; l<MasivaIzmers; l++)
