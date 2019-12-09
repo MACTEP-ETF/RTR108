abbd0785a961bf6cf11d1ec13291718c225ce114
Vladimirs <vfedorovich@yandex.ru>
2019-12-10 01:23:08 +0200

    20191210_01_22

---
+++ darbi/L16/dg16.py
@@ -0,0 +1,49 @@
+Python 3.6.9 (default, Nov  7 2019, 10:44:02) 
+[GCC 8.3.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+2+6
+8
+>>> 2*7
+14
+>>> 12/3
+4.0
+>>> 12%5
+2
+>>> 15-21
+-6
+>>> pow(2,5)
+32
+>>> type(pow(2,100))
+<class 'int'>
+>>> 2^100
+102
+>>> pow(2,100)
+1267650600228229401496703205376
+>>> type
+<class 'type'>
+>>> type(2*100)
+<class 'int'>
+>>> type(pow(2,100))
+<class 'int'>
+>>> man type
+SyntaxError: invalid syntax
+>>> cat type
+SyntaxError: invalid syntax
+>>> a=2
+>>> b=3
+>>> a+b
+5
+>>> a*b
+6
+>>> a/b
+0.6666666666666666
+>>> a%b
+2
+>>> pow(a,b)
+8
+>>> type(pow(a,b))
+<class 'int'>
+>>> type(pow(2,10000))
+<class 'int'>
+>>> type(pow(a,10000000000000000000))
