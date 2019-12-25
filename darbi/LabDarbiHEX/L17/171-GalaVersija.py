d6b48bf5b2aa10662e500f33acb3d08a39a95456
Vladimirs <vfedorovich@yandex.ru>
2019-12-25 12:22:53 +0200

    20191225_12_22

--- darbi/LabDarbiHEX/L17/171-GalaVersija.py
+++ darbi/LabDarbiHEX/L17/171-GalaVersija.py
@@ -2,38 +2,40 @@
 # Fails: 170.py
 # Autors: Vladimirs Fedorovičs
 # Apliecības numurs: 041RDB182
-# Datums: 24.12.2019
-# Sagatave funkcijas saknes meklēšanai ar dihotomijas metodi
+# Datums: 25.12.2019
+# Funkcijas cos(x)=0 saknes meklēšana ar dihotomijas metodi
 
 from math import cos, fabs
 from time import sleep
-
 def f(x):
     return cos(x)
 
-# Definējam argumenta x robežas:
-a = -1
-b = 1
+# Lietotājs definējam argumenta x robežas:
+print("Lūdzu, ievadiet argumenta x robežas [a, b]")
+a = input("a = ")
+b = input("b = ")
 
 # Aprēķinam funkcijas vērtības dotajos punktos:
-funa = f(a)-1
-funb = f(b)-1
+funa = f(a)
+funb = f(b)
 
 # Pārbaudam, vai dotajā intervālā ir saknes:
 if ( funa * funb > 0.0 ):
     print ("Dotajā intervālā [%s, %s] sakņu nav" %(a,b))
     sleep(1); exit() # Ziņo uz ekrāna, gaida 1 sec. un darbu pabeidz
 else:
-    print ("Dotajā intervālā sakne(s) ir!")
+    print ("Dotajā intervālā sakne(s) ir! Bet programma uzrādīs vienu no divām iespejamām (kompleksā programma ir gatava 2.LabDarba - roots ietvaros)")
 
 # Definējam precizitāti, ar kādu meklēsim sakni:
-deltax = 0.01
-
+deltax = 0.0001
+k = 0
 # Sašaurinam saknes meklēšanas robežas:
 while ( fabs(b-a) > deltax ):
-    x = (a+b)/2; funx = f(x)-1
+    k=k+1
+    x = (a+b)/2.; funx = f(x)
     if ( funa*funx < 0. ):
         b = x
     else:
         a = x
-print ("Sakne ir: ", (x))
+y=f(x)
+print ("x = %f. f(x) = %f. Iterāciju skaits, k = %d\n" %(x,y,k))
---
+++ darbi/LabDarbiHEX/L17/dg171.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Fails: 170.py
+# Autors: Vladimirs Fedorovičs
+# Apliecības numurs: 041RDB182
+# Datums: 25.12.2019
+# Funkcijas cos(x)=0 saknes meklēšana ar dihotomijas metodi
+
+from math import cos, fabs
+from time import sleep
+def f(x):
+    return cos(x)
+
+# Lietotājs definējam argumenta x robežas:
+print("Lūdzu, ievadiet argumenta x robežas [a, b]")
+a = input("a = ")
+b = input("b = ")
+
+# Aprēķinam funkcijas vērtības dotajos punktos:
+funa = f(a)
+funb = f(b)
+
+# Pārbaudam, vai dotajā intervālā ir saknes:
+if ( funa * funb > 0.0 ):
+    print ("Dotajā intervālā [%s, %s] sakņu nav" %(a,b))
+    sleep(1); exit() # Ziņo uz ekrāna, gaida 1 sec. un darbu pabeidz
+else:
+    print ("Dotajā intervālā sakne ir!")
+
+# Definējam precizitāti, ar kādu meklēsim sakni:
+deltax = 0.0001
+k = 0
+# Sašaurinam saknes meklēšanas robežas:
+while ( fabs(b-a) > deltax ):
+    k=k+1
+    x = (a+b)/2.; funx = f(x)
+    if ( funa*funx < 0. ):
+        b = x
+    else:
+        a = x
+y=f(x)
+print ("x = %f. f(x) = %f. Iterāciju skaits, k = %d\n" %(x,y,k))
