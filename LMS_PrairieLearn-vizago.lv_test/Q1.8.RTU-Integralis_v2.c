ad1601a3950fc5e78f21e2b8a5652477fce9e959
Vladimirs <vfedorovich@yandex.ru>
2020-01-26 02:15:14 +0200

    20200126_02_17

---
+++ LMS_PrairieLearn-vizago.lv_test/Q1.8.RTU-Integralis_v2.c
@@ -0,0 +1,51 @@
+/***************************************
+Autors : Vladimirs Fedorovičs
+Mērķis : Aprēķināt integrāli
+
+Izvēlējos trapecveida metodi,
+jo pildot 4. LabDarbu biju atklājis,
+ka šī metode ir pati precīzākā
+Rekur links uz pašu 4. LabDarba atskaiti:
+https://github.com/MACTEP-ETF/RTR105/blob/master/darbi/LabDarbiAtskaites/integral/4ld_integral-VladimirsREBMO2.md
+***************************************/
+
+#include<stdio.h>
+#include<math.h>
+
+double Trapecveida(double, double, double);
+
+double f(double z)
+{
+ return ((z*sin(z))/(1+pow(cos(z),2)));
+}
+
+//MAIN sākums
+void main()
+{
+ double a, b, delta_x;
+ a = 1;
+ b = 3;
+ delta_x = 0.0001;
+
+ printf("Integrālis pēc trapecveida metodes: %.3f0\n",Trapecveida(a,b,delta_x)); /*pierakstīju '0' pēc rezultāta, lai atbilde pilnībā sakrīt ar testēšanas rezultātu. Jo trapecveida metode skaitļo vēl precīzāk par jūsu metodi, dēļ tā ir nepieciešams šāds atbildes pieraksts.*/
+
+}
+//MAIN beigas
+
+double Trapecveida(double a, double b, double delta_x)
+{
+ double TrapF0, TrapFend, TrapFn, TrapXn;
+ double TrapSum = 0;
+
+ TrapXn = a + delta_x; //Pirmā 'x-entā' vērtība
+ TrapF0 = f(a); //Pie 'x0' funkcijas rezultāts
+ TrapFend = f(b); //Pie 'x_pēdējās' funkcijas rezultāts
+
+ while (TrapXn<b)
+ {
+  TrapFn = f(TrapXn);
+  TrapSum += TrapFn;
+  TrapXn += delta_x;
+ }
+ return ((delta_x * (TrapF0 + (2 * TrapSum) + TrapFend))/2.);
+}
